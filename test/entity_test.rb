9021501f8769c77121005067d3d021fd708ac92a
ippachi <ippachi1018@gmail.com>
2022-04-10 20:45:24 +0900

    test(entity): refactor entity test

--- test/entity_test.rb
+++ test/entity_test.rb
@@ -6,36 +6,41 @@ require "test_helper"
 
 module Kapsule
   class EntityTest < Test::Unit::TestCase
+    extend T::Sig
     def setup
-      @class = Class.new(Kapsule::Entity)
+      @class = T.let(T.unsafe(Class.new(Kapsule::Entity)), T.nilable(T.class_of(Kapsule::Entity)))
+      target_class.property :name, String
+      target_class.property :email, String
+    end
+
+    sig { returns(T.class_of(Kapsule::Entity)) }
+    def target_class
+      T.must(@class)
     end
 
     def test_initialize
       assert_nothing_raised do
-        @class.new
+        target_class.new
       end
     end
 
     class PropertyTest < self
       def test_add_property_definitions
-        @class.property :name, String
-        assert_equal String, @class.property_definitions[:name]
+        assert_equal String, target_class.property_definitions[:name]
       end
 
-      def test_define_setter_and_getter
-        @class.property :name, String
-        instance = @class.new(name: "user name")
-        name = "changed name"
-        instance.name = name
-        assert_equal name, instance.name
+      def test_initialize
+        assert_nothing_raised do
+          target_class.new(name: "name", email: "email")
+        end
       end
     end
 
     def test_serialize
-      @class.property :name, String
-      name = "user name"
-      instance = @class.new(name: name)
-      assert_equal({ name: name }, instance.serialize)
+      name = "name"
+      email = "email"
+      instance = target_class.new(name: name, email: email)
+      assert_equal({ name: name, email: email }, instance.serialize)
     end
   end
 end
